1c54c77c9eab94db3fa96b750fe8e0efce7b4edc
Hugo Diniz <hhldiniz@gmail.com>
2018-03-26 12:51:52 -0300

    criação de view para cadastro de usuário e ligação com controller

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const controllerClass = require('../utils/DBController.js');
+const dbController = new controllerClass("localhost",27017, "", "", "cadastro_simples");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
-router.post('/', function (req, res) {
+router.get('/create', function (req, res) {
+  res.render("users",{title: "Incluir Usuário"});
+});
 
+router.post('/create', function(req,res){
+  let nome_usuario = req.body.nome;
+  let email_usuario = req.body.email;
+  let idade_usuario = req.body.idade;
+  dbController.connect(()=>{
+    dbController.insert(r=>{
+      if(r.nInserted > 0)
+      {
+        res.redirect("index");
+      }
+    },"users",{nome: nome_usuario, email: email_usuario, idade: idade_usuario});
+  });
 });
 
 module.exports = router;
